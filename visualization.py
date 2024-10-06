368778d00f5f2a10391357f1d535187c0543b422
A_Nama <145112688+A-Nama@users.noreply.github.com>
2024-10-06 19:00:35 +0530

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import streamlit as st
 
-def plot_solarpunk_ecosystem(vegetation, water, energy):
-    labels = ['Vegetation', 'Water', 'Energy']
-    values = [vegetation, water, energy]
+def plot_solarpunk_ecosystem(vegetation, water, weather):
+    labels = ['vegetation', 'Water', 'weather']
+    values = [vegetation, water, weather]
 
     fig, ax = plt.subplots()
-    ax.bar(labels, values, color=['blue', 'blue', 'blue'])
+    ax.bar(labels, values, color=['green', 'blue', 'yellow'])
     
     ax.set_ylim(0, 100)
     ax.set_ylabel('Levels (%)')
